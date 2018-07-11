b60c87858890051a161e7aed19e2de0146a7abb3
Chris <chrislo.7@live.com>
2018-07-11 16:38:07 +0000

    added loopylighthouse assignment

---
+++ loopy.js
@@ -0,0 +1,17 @@
+var bottom = 100;
+var top = 200;
+var three = 'Loopy';
+var four = 'Lighthouse';
+var twelve = 'loopylighthouse';
+
+for (i = bottom; i <= top; i++) {
+  if (i % 3 === 0 && i % 12 !== 0) {
+    console.log(three);
+  } else if (i % 4 === 0 && i % 12 !== 0) {
+    console.log(four);
+  } else if (i % 12 === 0) {
+    console.log(twelve);
+  } else {
+    console.log(i);
+  }
+}
